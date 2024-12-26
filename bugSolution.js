5b928aac3cf85a928e17b0b6d088e751a4542d2f
BugHunter-X <askzenai@gmail.com>
2024-12-26 10:14:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,7 @@
+```javascript
+<div class="flex flex-wrap">
+  <div class="bg-red-500 p-4 w-1/2"></div>
+  <div class="bg-blue-500 p-4 w-1/2"></div>
+</div>
+```
+By adding `flex-wrap`, we allow the flex items to wrap onto the next line if necessary, preventing the overflow.  Consider using `flex-wrap-reverse` to reverse the wrapping order.
